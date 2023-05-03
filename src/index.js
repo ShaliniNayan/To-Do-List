8d032a5d9ab45d006f5c3b382d556db3271002dd
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-05-03 16:32:35 +0530

    Update html css & js file

--- src/index.js
+++ src/index.js
@@ -1,73 +1,47 @@
-/* eslint-disable no-unused-vars */
-import _ from 'lodash';
-import AddItems from './print';
-// import printMe from './print.js';
-import './style.css';
+const todoList = document.getElementById('todo-list');
+const inputTask = document.getElementById('input-task');
+const addBtn = document.getElementById('add-btn');
 
-const form = document.getElementById('todo-form');
-const input = document.getElementById('todo-input');
-const button = document.getElementById('todo-button');
-const list = document.getElementById('todo-list');
-const clearButton = document.getElementById('todo-clear-button');
+let tasks = [];
 
-let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-
-function createTask(task, index, completed) {
-  return `
-  <li class="todo-item ${completed ? 'completed' : ''}" data-index="${index}">
-    <input type="checkbox" class="todo-checkbox" ${completed ? 'checked' : ''} />
-    <span class="todo-description">${task}</span>
-    <button class="todo-delete">x</button>
-  </li>
-  `;
+function displayTasks() {
+  todoList.innerHTML = '';
+  tasks.forEach((task, index) => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <span>${task}</span>
+      <button class="delete-btn" data-index="${index}">Delete</button>
+      <button class="complete-btn" data-index="${index}">Complete</button>
+    `;
+    if (task.completed) {
+      li.classList.add('completed');
+    }
+    todoList.appendChild(li);
+  });
 }
 
-function renderTasks() {
-  list.innerHTML = tasks.map((task, index) => createTask(task.description, index, task.completed)).join('');
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-}
-
-function addTask(event) {
-  event.preventDefault();
-  if (!input.value.trim()) {
-    return;
+addBtn.addEventListener('click', () => {
+  const task = inputTask.value;
+  if (task) {
+    tasks.push({
+      task,
+      completed: false,
+    });
+    displayTasks();
+    inputTask.value = '';
   }
-  const task = {
-    description: input.value.trim(),
-    completed: false,
-    index: tasks.length + 1
-  };
-  tasks.push(task);
-  renderTasks();
-  input.value = '';
-}
+});
 
-function completeTask(event) {
-  if (!event.target.matches('.todo-checkbox')) {
-    return;
+todoList.addEventListener('click', (event) => {
+  if (event.target.className === 'delete-btn') {
+    const index = event.target.getAttribute('data-index');
+    tasks.splice(index, 1);
+    displayTasks();
+  } else if (event.target.className === 'complete-btn') {
+    const index = event.target.getAttribute('data-index');
+    tasks[index].completed = true;
+    displayTasks();
   }
-  const index = event.target.parentNode.dataset.index;
-  tasks[index].completed = event.target.checked;
-  renderTasks();
-}
+});
 
-function deleteTask(event) {
-  if (!event.target.matches('.todo-delete')) {
-    return;
-  }
-  const index = event.target.parentNode.dataset.index;
-  tasks.splice(index, 1);
-  renderTasks();
-}
-
-function clearCompleted() {
-  tasks = tasks.filter(task => !task.completed);
-  renderTasks();
-}
-
-form.addEventListener('submit', addTask);
-list.addEventListener('change', completeTask);
-list.addEventListener('click', deleteTask);
-clearButton.addEventListener('click', clearCompleted);
-
-renderTasks();
+displayTasks();
