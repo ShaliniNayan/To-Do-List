8bf8c40aed6f9cae8bc2db48b682d59aba6991c8
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-05-05 08:41:09 +0530

    Fix linter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./css/style.css";
-import "./css/font-awesome.css"
-import { clearBtn, todoInput } from "./module/newFunctionalities.js";
-import completeToDo from "./module/app.js";
+import './css/style.css';
+import './css/font-awesome.css';
+import './module/newFunctionalities.js';
+import './module/app.js';
